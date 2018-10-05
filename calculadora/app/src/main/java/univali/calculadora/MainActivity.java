8d1321a5fe8b90278ac6fe542eba5a93cdd4a242
Diogo Agenor Marchi <6427626@univali.br>
2018-10-05 10:55:14 -0300

    pronto a calculadora

--- calculadora/app/src/main/java/univali/calculadora/MainActivity.java
+++ calculadora/app/src/main/java/univali/calculadora/MainActivity.java
@@ -2,6 +2,12 @@ package univali.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,29 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void fazerAParadaAcontecer(View elementoClicado){
-        EditText altura = findViewById(R.id.etAltura);
-        EditText peso = findViewById(R.id.etPeso);
-        TextView imc = findViewById(R.id.tvImc);
+    public void calcularSalario(View elementoClicado){
+        EditText salarioParcial = findViewById(R.id.etSalario);
+        EditText dias = findViewById(R.id.etDia);
+        TextView salario = findViewById(R.id.tvSalario);
 
-        String alturaDigitada = altura.getText().toString();
-        String pesoDigitado = peso.getText().toString();
+        String salarioDigitado = salarioParcial.getText().toString();
+        String diasDigitado = dias.getText().toString();
 
-        if(alturaDigitada.equals("")  || pesoDigitado.equals("")){
-            altura.setError("Informe a altura");
-            peso.setError("Informe o peso");
+        if(salarioDigitado.equals("")  || diasDigitado.equals("")){
+            salarioParcial.setError("Informe a altura");
+            dias.setError("Informe o peso");
             return;
         }
 
         try{
-            double alturaEmDouble = Double.parseDouble(alturaDigitada);
-            double pesoEmDouble = Double.parseDouble(pesoDigitado);
+            double salarioParcialEmDouble = Double.parseDouble(salarioDigitado);
+            double diasEmDouble = Double.parseDouble(diasDigitado);
 
-            double resultadoImc = pesoEmDouble/(alturaEmDouble*alturaEmDouble);
+            double resultadoSalario = salarioParcialEmDouble*diasEmDouble;
 
-            imc.setText( "IMC: "+resultadoImc );
-
-            imc.setVisibility(View.VISIBLE);
+            salario.setText( "Salario: "+resultadoSalario );
+            salario.setVisibility(View.VISIBLE);
         }catch (Exception e){
             Toast mensagemErro = Toast.makeText(this, "Algo errado aconteceu", Toast.LENGTH_LONG);
             mensagemErro.show();
