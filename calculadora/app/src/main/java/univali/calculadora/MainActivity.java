d1ebe50231d979206404032be33d9dccdfb55ed1
Diogo Agenor Marchi <6427626@univali.br>
2018-10-05 10:06:37 -0300

    pronto tela contato

--- calculadora/app/src/main/java/univali/calculadora/MainActivity.java
+++ calculadora/app/src/main/java/univali/calculadora/MainActivity.java
@@ -10,4 +10,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void fazerAParadaAcontecer(View elementoClicado){
+        EditText altura = findViewById(R.id.etAltura);
+        EditText peso = findViewById(R.id.etPeso);
+        TextView imc = findViewById(R.id.tvImc);
+
+        String alturaDigitada = altura.getText().toString();
+        String pesoDigitado = peso.getText().toString();
+
+        if(alturaDigitada.equals("")  || pesoDigitado.equals("")){
+            altura.setError("Informe a altura");
+            peso.setError("Informe o peso");
+            return;
+        }
+
+        try{
+            double alturaEmDouble = Double.parseDouble(alturaDigitada);
+            double pesoEmDouble = Double.parseDouble(pesoDigitado);
+
+            double resultadoImc = pesoEmDouble/(alturaEmDouble*alturaEmDouble);
+
+            imc.setText( "IMC: "+resultadoImc );
+
+            imc.setVisibility(View.VISIBLE);
+        }catch (Exception e){
+            Toast mensagemErro = Toast.makeText(this, "Algo errado aconteceu", Toast.LENGTH_LONG);
+            mensagemErro.show();
+        }
+
+
+
+    }
 }
